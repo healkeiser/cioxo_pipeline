828062e9e8b17cecf7669f2f15dee6d23c42a0fa
Valentin Beaumont <valentin.onze@gmail.com>
2021-10-20 10:20:46 +0100

    Pre-release

---
+++ all/cioxo_main_defineRoot_createExecutable.bat
@@ -0,0 +1,1 @@
+pyinstaller --onefile --icon=C:\Users\Valen\Documents\PROJECTS\.pipeline\all\ui\graphics\logos\cioxoLogo_root.ico C:\Users\Valen\Documents\PROJECTS\.pipeline\all\cioxo_main_defineRoot.py
